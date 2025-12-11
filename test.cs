afccf012adcd288ae557fe7d32f54073ed6f3ac3
okofka <nazar.dmytroo@gmail.com>
2025-12-11 23:01:10 +0200

    Add new class

---
+++ test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPM_modul
+{
+    internal class test
+    {
+    }
+}
